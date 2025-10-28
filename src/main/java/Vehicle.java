007620b9df6e124088eed4331e710153136f5ed4
eriinys <eriinys@duck.com>
2025-10-28 10:09:11 -0500

    added getValue method to Vehicle

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -1,3 +1,6 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Vehicle extends Assets{
     private String makeModel;
     private int year;
@@ -33,5 +36,30 @@ public class Vehicle extends Assets{
     }
     //endregion
 
+    public double getValue(){
+        LocalDate now = LocalDate.now();
+        double age = (Double.parseDouble(getDateAcquired()) - year) + now.getYear();
+        double rate;
+        double reducedValue;
+        if (age <= 3) {
+            rate = 0.03;
+            reducedValue = getOriginalCost() - (getOriginalCost() * (age * rate));
+        } else if (age <=6 && age >= 4) {
+            rate = 0.06;
+            reducedValue = getOriginalCost() - (getOriginalCost() * (age * rate));
+        } else if (age <=10 && age >= 7) {
+            rate = 0.08;
+            reducedValue = getOriginalCost() - (getOriginalCost() * (age * rate));
+        } else {
+            reducedValue = getOriginalCost() - 1000;
+        }
+
+        if (!makeModel.toLowerCase().contains("honda") && !makeModel.toLowerCase().contains("toyota")) {
+            if (odometer > 100000) {
+                reducedValue -= reducedValue - (reducedValue * 0.25);
+            }
+        }
+        return reducedValue;
+    }
 
 }
